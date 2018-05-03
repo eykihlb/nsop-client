02452fb23de4d69e18de673f88b4cf3368c21837
zyw090111@163.com <zyw090111@163.com>
2018-05-03 18:09:06 +0800

    first init

---
+++ nsop-client-main/src/main/java/com/mydao/nsop/client/NsopClientMainApplication.java
@@ -0,0 +1,12 @@
+package com.mydao.nsop.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NsopClientMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NsopClientMainApplication.class, args);
+	}
+}
---
+++ nsop-client-main/src/test/java/com/mydao/nsop/client/main/NsopClientMainApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mydao.nsop.client.main;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NsopClientMainApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
