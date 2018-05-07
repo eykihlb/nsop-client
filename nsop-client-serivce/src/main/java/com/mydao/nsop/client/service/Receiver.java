2f2981befb8fb0d3d0cfbfa691883d20094b1890
zyw090111@163.com <zyw090111@163.com>
2018-05-07 19:09:50 +0800

    添加监听例子

---
+++ nsop-client-serivce/src/main/java/com/mydao/nsop/client/service/Receiver.java
@@ -0,0 +1,63 @@
+package com.mydao.nsop.client.service;
+
+import com.mydao.nsop.client.common.Constants;
+import com.rabbitmq.client.Channel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+/**
+ * @author ZYW
+ * @date 2018/5/7
+ */
+@Component
+public class Receiver {
+    private static final Logger log = LoggerFactory.getLogger(Receiver.class);
+    /**
+     * FANOUT广播队列监听一.
+     *
+     * @param message the message
+     * @param channel the channel
+     * @throws IOException the io exception  这里异常需要处理
+     */
+    @RabbitListener(queues = {Constants.TOPIC_TSX_JOURNEY})
+    public void on(Message message, Channel channel) throws IOException {
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        log.info("FANOUT_QUEUE_A "+new String(message.getBody()));
+        channel.basicNack(message.getMessageProperties().getDeliveryTag(), true,true);
+    }
+
+    /**
+     * FANOUT广播队列监听二.
+     *
+     * @param message the message
+     * @param channel the channel
+     * @throws IOException the io exception   这里异常需要处理
+     */
+    /*@RabbitListener(queues = {"FANOUT_QUEUE_B"})
+    public void t(Message message, Channel channel) throws IOException {
+        channel.basicAck(message.getMessageProperties().getDeliveryTag(), true);
+        log.debug("FANOUT_QUEUE_B "+new String(message.getBody()));
+    }*/
+
+    /**
+     * DIRECT模式.
+     *
+     * @param message the message
+     * @param channel the channel
+     * @throws IOException the io exception  这里异常需要处理
+     */
+    /*@RabbitListener(queues = {"DIRECT_QUEUE"})
+    public void message(Message message, Channel channel) throws IOException {
+        channel.basicAck(message.getMessageProperties().getDeliveryTag(), true);
+        log.debug("DIRECT "+new String (message.getBody()));
+    }*/
+}
--- nsop-client-serivce/src/main/java/com/mydao/nsop/client/service/VehicleDriveInService.java
+++ nsop-client-serivce/src/main/java/com/mydao/nsop/client/service/VehicleDriveInService.java
@@ -19,14 +19,14 @@ public class VehicleDriveInService {
 
     @Async
     public void test(){
-        try {
+        /*try {
             while (true) {
                 Thread.sleep(1000);
                 System.out.println("test-------------------");
             }
         } catch (InterruptedException e) {
             e.printStackTrace();
-        }
+        }*/
     }
 
     /**
@@ -35,7 +35,10 @@ public class VehicleDriveInService {
     @Async
     public void test2() {
         System.out.println("Sender : Hello Word！");
-        rabbitTemplate.convertAndSend(Constants.TOPIC_TSX_JOURNEY,"Hello Word！");
+        for(int i = 0; i < 10; i++) {
+            rabbitTemplate.convertAndSend(Constants.TOPIC_TSX_JOURNEY,"Hello Word！" + i);
+        }
+
     }
 
     /**
@@ -43,10 +46,10 @@ public class VehicleDriveInService {
      */
     @Async
     public void test3() {
-        while(true) {
-            Message receive = rabbitTemplate.receive(Constants.TOPIC_TSX_JOURNEY,-1);
-            String s = new String(receive.getBody());
-            System.out.println("接收：" + s);
-        }
+//        while(true) {
+//            Message receive = rabbitTemplate.receive(Constants.TOPIC_TSX_JOURNEY,-1);
+//            String s = new String(receive.getBody());
+//            System.out.println("接收：" + s);
+//        }
     }
 }
