93874c79301caab2b4b088ee8e16a673ea9f2e00
eykihlb <eykihlb@163.com>
2018-05-08 15:01:57 +0800

    添加常量数据

--- nsop-client-common/src/main/java/com/mydao/nsop/client/common/Constants.java
+++ nsop-client-common/src/main/java/com/mydao/nsop/client/common/Constants.java
@@ -8,4 +8,14 @@ public class Constants {
      * 车辆驶入Topic
      */
     public static final String TOPIC_TSX_JOURNEY = "topic_tsx_journey";
+
+    /**
+     *  车辆黑名单Topic
+     */
+    public static final String TOPIC_TSX_BLACKVEH = "topic_tsx_blackveh";
+
+    /**
+     * 车辆白名单Topic
+     */
+    public static final String TOPIC_TSX_WHITEVEH = "topic_tsx_whiteveh";
 }
