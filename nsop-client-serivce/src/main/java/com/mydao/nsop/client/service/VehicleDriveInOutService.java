8ec9af838fa3b4cc8e18fe3fe9b142ec91d38254
eykihlb <eykihlb@163.com>
2018-05-10 19:54:16 +0800

    文件上传下载Service

--- nsop-client-serivce/src/main/java/com/mydao/nsop/client/service/VehicleDriveInOutService.java
+++ nsop-client-serivce/src/main/java/com/mydao/nsop/client/service/VehicleDriveInOutService.java
@@ -56,7 +56,7 @@ public class VehicleDriveInOutService {
         System.out.println("Sender : Hello Word！");
         for(int i = 0; i < 10; i++) {
             System.out.println("message" + i);
-            rabbitTemplate.convertAndSend(Constants.ENTRY_QUEUE,"{'laneNo':'65000115E0','passTime':'445555555','plateNo':'京A12345-1','src':'00','feature':'00_00_00','cameraId':'000001','passSeq':'2000','clientId':'000001','clientSeq':'1'}");
+            //rabbitTemplate.convertAndSend(Constants.ENTRY_QUEUE,"{'laneNo':'65000115E01','passTime':'445555555','plateNo':'京A12345-1','src':'00','feature':'00_00_00','cameraId':'000001','passSeq':'2000','clientId':'000001','clientSeq':'1'}");
         }
 
     }
