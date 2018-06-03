f9dce9ff82b1117b63cd892c919f2f72326dab85
zhuyanwei <zyw090111@163.com>
2018-06-03 22:13:14 +0800

    配置OAuthRestTemplate的链接方式为HttpClient

--- nsop-client-main/src/main/java/com/mydao/nsop/client/main/StartAllThread.java
+++ nsop-client-main/src/main/java/com/mydao/nsop/client/main/StartAllThread.java
@@ -38,7 +38,7 @@ public class StartAllThread {
         createSubscriptionAndQueue.createSubQueue();
         //初始化拉取全量黑白名单数据
         systemInit.systemInit();
-        systemInit.aaa();
+        systemInit.restTemplateTest();
 //        vehicleDriveInService.test();
 //        vehicleDriveInService.test2();
 //        vehicleDriveInService.test3();
