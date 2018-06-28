aa2d935b26bac11f2bcb80f28ac1eebf3047ed38
zyw090111@163.com <zyw090111@163.com>
2018-06-28 10:12:51 +0800

    修改配置位置

--- nsop-client-main/src/main/java/com/mydao/nsop/client/NsopClientMainApplication.java
+++ nsop-client-main/src/main/java/com/mydao/nsop/client/NsopClientMainApplication.java
@@ -5,14 +5,8 @@ import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.task.AsyncTaskExecutor;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-
-import java.util.concurrent.RejectedExecutionHandler;
-import java.util.concurrent.ThreadPoolExecutor;
 
 @SpringBootApplication
 @EnableAsync
@@ -20,26 +14,6 @@ import java.util.concurrent.ThreadPoolExecutor;
 public class NsopClientMainApplication {
 
 	private final static Logger LOGGER = Logger.getLogger(NsopClientMainApplication.class);
-	/**
-	 * 自定义异步线程池
-	 * @return
-	 */
-	@Bean
-	public AsyncTaskExecutor taskExecutor() {
-		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-		executor.setThreadNamePrefix("Client-Executor");
-		executor.setMaxPoolSize(10);
-		executor.setCorePoolSize(6);
-		// 设置拒绝策略
-		executor.setRejectedExecutionHandler(new RejectedExecutionHandler() {
-			@Override
-			public void rejectedExecution(Runnable r, ThreadPoolExecutor executor) {
-				// .....
-			}
-		});
-		// 使用预定义的异常处理类
-		return executor;
-	}
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext run = SpringApplication.run(NsopClientMainApplication.class, args);
@@ -47,4 +21,5 @@ public class NsopClientMainApplication {
 		thriftServer.startServer();
 		LOGGER.info("启动成功！");
 	}
+
 }
---
+++ nsop-client-serivce/src/main/java/com/mydao/nsop/client/config/ScheduledConfig.java
@@ -0,0 +1,49 @@
+package com.mydao.nsop.client.config;
+
+import org.apache.commons.lang3.concurrent.BasicThreadFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.AsyncTaskExecutor;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+
+import java.util.concurrent.*;
+
+/**
+ * @author ZYW
+ * @date 2018/6/26
+ */
+@Configuration
+public class ScheduledConfig implements SchedulingConfigurer {
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+        taskRegistrar.setScheduler(setTaskExecutors());
+    }
+
+    /**
+     * 自定义异步线程池
+     * @return
+     */
+    @Bean
+    public AsyncTaskExecutor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setThreadNamePrefix("Client-Executor");
+        executor.setMaxPoolSize(10);
+        executor.setCorePoolSize(6);
+        // 设置拒绝策略
+        executor.setRejectedExecutionHandler(new RejectedExecutionHandler() {
+            @Override
+            public void rejectedExecution(Runnable r, ThreadPoolExecutor executor) {
+                // .....
+            }
+        });
+        // 使用预定义的异常处理类
+        return executor;
+    }
+
+    @Bean
+    public Executor setTaskExecutors(){
+        return new ScheduledThreadPoolExecutor(4,new BasicThreadFactory.Builder().namingPattern("scheduled-pool-%d").daemon(true).build());
+    }
+}
